911ef6a1a1755ff49fdc4abd5cea1f229151644f
Larry Clement <lclement@calbaptist.edu>
2024-02-25 15:23:47 -0800

    Prior to class on 02/27/2024

---
+++ B/Employee/src/EmployeeClient.java
@@ -0,0 +1,20 @@
+public class EmployeeClient {
+    public static void main(String[] args) {
+
+//     Consider the following employee regulations:
+//        Employees work 40 hours / week.
+//                Employees make $40,000 per year, except legal secretaries who make $5,000 extra per year ($45,000 total), and marketers who make $10,000 extra per year ($50,000 total).
+//                Employees have 2 weeks of paid vacation leave per year, except lawyers who get an extra week (a total of 3).
+//        Employees should use a yellow form to apply for leave, except for lawyers who use a pink form.
+//
+//     Each type of employee has some unique behavior:
+//        Lawyers know how to sue.
+//                Marketers know how to advertise.
+//        Secretaries know how to take dictation.
+//        Legal secretaries know how to prepare legal documents.
+
+        Employee e = new Employee();
+
+        // do stuff using the Employee (and child classes) here...
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# 02.27-02.29.2024.InClassExamples
+
+In class examples for the week of 02.27-02.29.2024
